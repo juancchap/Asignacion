3e1f5739686a54efc6da3adaaee6a33c0512bcf7
JUAN CARLOS <JUAN CARLOS@LAPTOP-NF1JBM48>
2024-01-22 11:47:26 -0600

    Arimetricos clase del dia lunes 22 de enero

---
+++ src/asignacion/Asignacion.java
@@ -0,0 +1,14 @@
+package asignacion;
+
+public class Asignacion {
+
+    public static void main(String[] args) {
+        int numero = 10;
+        //asignacion
+        numero-= 5;
+        // es igual que pusiera numero =numero+5
+        System.out.println("El resultado es: " + numero);
+
+    }
+
+}
